1612d2ada0850b8827ded29cab460ab951d78807
alaa-mohamed <333alaamo@gmail.com>
2026-05-30 15:16:48 +0300

    chore(dto/response): add new factory method to handle failed response

--- src/main/java/com/ecommerce/api/dto/response/ApiResponse.java
+++ src/main/java/com/ecommerce/api/dto/response/ApiResponse.java
@@ -1,13 +1,17 @@
 package com.ecommerce.api.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import java.time.LocalDateTime;
 
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record ApiResponse<T> (
         boolean success,
         String message,
         T data,
         LocalDateTime timestamp
-){
+) {
     public static <T> ApiResponse<T> success(String message, T data) {
 
         return new ApiResponse<>(true, message, data, LocalDateTime.now());
@@ -15,5 +19,13 @@ public record ApiResponse<T> (
 
     public static <T> ApiResponse<T> success(T data) {
         return success("operation successful", data);
+    }
+
+    public static <T> ApiResponse<T> error(String message) {
+        {
+
+            return new ApiResponse<>(false, message, null, LocalDateTime.now());
+        }
+
     }
 }
